0418eb72602a7d5b1f36b3303320be70c2910131
ajin705575497 <root@bogon.localdomain>
2018-07-13 07:27:41 -0700

    file

--- tcpexercise/session1/helloWorld1.1/client/hello_client.c
+++ tcpexercise/session1/helloWorld1.1/client/hello_client.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
         printf("Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
     }
-
+    //创建套接字，但此时套接字并不马上分为服务器端和客户端。如果紧接着调用bind、listen函数，将成为服务器端套接字；如果调用connect函数，将成为客户端套接字
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if(sock == -1)
         error_handling("socket() error");
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
-    
+    //调用connect函数向服务器端发送连接请求
     if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
         error_handling("connect() error!");
     
